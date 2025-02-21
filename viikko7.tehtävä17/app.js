129312fc85ff9bf3d834e86cb06d2891d6cc2bac
mkorvenpa <katariina.korvenpaa@gmail.com>
2025-02-21 14:33:07 +0200

    Viikon 7 tehtävät

---
+++ viikko7.tehtävä17/app.js
@@ -0,0 +1,21 @@
+    var express = require('express');
+    var path = require('path');
+    var cookieParser = require('cookie-parser');
+    var logger = require('morgan');
+
+    var bookRouter = require('./routes/book');
+    var borrowerRouter = require('./routes/borrower');
+
+    var app = express();
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/book', bookRouter);
+    app.use('/borrower', borrowerRouter);
+
+    module.exports = app;
+
---
+++ viikko7.tehtävä19/app.js
@@ -0,0 +1,63 @@
+    var express = require('express');
+    var path = require('path');
+    var cookieParser = require('cookie-parser');
+    var logger = require('morgan');
+
+    var bookRouter = require('./routes/book');
+    var borrowerRouter = require('./routes/borrower');
+    var userRouter = require('./routes/user');
+    var loginRouter = require('./routes/login');
+
+    const db = require('./database');
+    const bcrypt = require('bcryptjs');
+    const basicAuth = require('express-basic-auth');
+
+    var app = express();
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/book', bookRouter);
+
+    app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
+
+    app.use('/borrower', borrowerRouter);
+    app.use('/user', userRouter);
+    app.use('/login', loginRouter);
+
+    function myAuthorizer(username, password,cb){
+      db.query('SELECT password FROM user WHERE username = ?',[username], 
+        function(dbError, dbResults, fields) {
+          if(dbError){
+                response.json(dbError);
+              }
+          else {
+            if (dbResults.length > 0) {
+              bcrypt.compare(password,dbResults[0].password, 
+                function(err,res) {
+                  if(res) {
+                    console.log("success");
+                    return cb(null, true);
+                  }
+                  else {
+                    console.log("wrong password");
+                    return cb(null, false);
+                  }			
+                  response.end();
+                }
+              );
+            }
+            else{
+              console.log("user does not exists");
+              return cb(null, false);
+            }
+          }
+        }
+      );
+    }
+
+    module.exports = app;
+
